29906f513e84f1acc977542a2277b41912744003
mairagee524 <garciamaira524@yahoo.com>
2020-11-13 22:52:27 -0800

    added button files and modified navbar code

---
+++ src/components/Navbar/MenuItem.js
@@ -0,0 +1,37 @@
+export const MenuItem = [
+    {
+        title: 'Home',
+        url: '#',
+        className: 'nav-links'
+    },
+    {
+        title: 'About Me',
+        url: '#',
+        className: 'nav-links'
+    },
+    {
+        title: 'Skills',
+        url: '#',
+        className: 'nav-links'
+    },
+    {
+        title: 'Portfolio',
+        url: '#',
+        className: 'nav-links'
+    },
+    {
+        title: 'Testimonials',
+        url: '#',
+        className: 'nav-links'
+    },
+    {
+        title: 'Contact Me',
+        url: '#',
+        className: 'nav-links'
+    },
+    {
+        title: 'Resume',
+        url: '#',
+        className: 'nav-links-mobile'
+    },
+]
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
+import { MenuItem } from './MenuItem';
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar-items">
-                <h1 className="navbar-logo">MG</h1>
-                <div className="menu-icon"></div>
+            <nav className='navbar-items'>
+                <h1 className='navbar-logo'>MG</h1>
+                <div className='menu-icon'></div>
                 <ul>
-                    <li></li>
+                    {MenuItem.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.className} href={item.url}
+                                >
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         )
     }
 }
+
+export default Navbar;
