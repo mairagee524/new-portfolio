521b71b5535318b1fcfe4c0accb43bb747669327
mairagee524 <garciamaira524@yahoo.com>
2020-11-13 22:33:06 -0800

    reorganized files and modified app.js code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
   
-import React, { Component } from "react";
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css"
 // import Header from "./components/Header";
 // import Portfolio from "./components/Portfolio";
 // import AboutMe from "./components/AboutMe";
@@ -8,21 +10,29 @@ import React, { Component } from "react";
 // import Footer from "./components/Footer";
 // import resumeData from "./resumeData";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-
-        {/* <Header resumeData={resumeData} />
-        <AboutMe resumeData={resumeData} />
-        <Portfolio resumeData={resumeData} />        
-        <Testimonials resumeData={resumeData}/>
-        <ContactUs resumeData={resumeData} />
-        <Footer resumeData={resumeData} /> */}
-      </div>
-    );
-  }
+function App () {
+  return (
+    <div classname="App">
+      <Navbar />
+    </div>
+  )
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello World</h1>
+
+//         {/* <Header resumeData={resumeData} />
+//         <AboutMe resumeData={resumeData} />
+//         <Portfolio resumeData={resumeData} />        
+//         <Testimonials resumeData={resumeData}/>
+//         <ContactUs resumeData={resumeData} />
+//         <Footer resumeData={resumeData} /> */}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
