b0710eaed770bf0ce2e406ebffef605a0ce33fc2
mairagee524 <garciamaira524@yahoo.com>
2020-11-13 22:39:37 -0800

    added starter code to navbar file

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,2 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar-items">
+                <h1 className="navbar-logo">MG</h1>
+                <div className="menu-icon"></div>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        )
+    }
+}
