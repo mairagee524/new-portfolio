f2a61c12f2f8e011e14379267a730f0ff4659746
mairagee524 <garciamaira524@yahoo.com>
2020-11-18 23:58:24 -0800

    added styling for navbar and button

---
+++ src/components/Button/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+const STYLES = [
+    'btn-primary',
+    'btn-outline'
+]
+
+const SIZES = [
+    'btn-md',
+    'btn-lg'
+]
+
+export const Button = ({
+    children,
+    type, 
+    onClick,
+    buttonStyle,
+    buttonSize
+}) => {
+    
+}
