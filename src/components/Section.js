4296185207e91a9693c92beb3d8c062ebf2d35ec
mairagee524 <garciamaira524@yahoo.com>
2020-11-23 23:16:49 -0800

    added section intro

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, from 'react';
 import { Button, ButtonGroup } from "@chakra-ui/react"
 
 function Welcome(props) {
     return (
-        <Button colorScheme="teal" size="lg">
-            Resume
-        </Button>
+        <div>
+            <h1> Hi, I am Maira Garcia, a bilingual Full-Stack Engineer</h1>
+            <h2> Based in Los Angeles, CA, I am passionate about building applications for the greater good. </h2>
+            <Button colorScheme="teal" size="lg">
+                Resume
+            </Button>
+        </div>
     )
 }
 
